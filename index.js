c8d68479f8890de43d4b4b7841c3f6290d7d3579
max <mkuchin@gmail.com>
2017-12-10 20:54:25 +1100

    encrypt/decrypt mode

--- index.js
+++ index.js
@@ -1,29 +1,65 @@
-var args = process.argv.slice(2);
 const bip39 = require('bip39');
 const scrypt = require('scrypt');
 const xor = require('buffer-xor');
 const qr = require('qrcode');
 const base58 = require('bs58');
-
+const crypto = require('crypto');
 //console.log(args);
-var password = args[0];
-var mnemonic = args[1];
+
+function usage() {
+  console.log('Usage: recovery-seed encrypt PASSWORD "MNEMONIC"');
+  console.log('Usage: recovery-seed decrypt PASSWORD SALT "MNEMONIC"');
+  process.exit();
+}
+var args = process.argv.slice(2);
 
 
-var seedHex = bip39.mnemonicToEntropy(mnemonic);
-console.log("seed    = " + seedHex);
-var seed = new Buffer(seedHex, 'hex');
-var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, "salt");
-console.log(pHash.toString('hex'));
 
-var encSeed = xor(seed, pHash);
-var encSeed58 = base58.encode(encSeed);
-console.log("encSeed = " + encSeed.toString('hex'));
-console.log("base58  = " + encSeed58);
+var mode = args[0];
+if(mode == 'encrypt') {
+  if(args.length < 3) {
+    usage();
+  }
 
-qr.toDataURL(encSeed58, function (err, url) {
-  console.log(url)
-})
+  var password = args[1];
+  var mnemonic = args[2];
 
-var encMnemonic = bip39.entropyToMnemonic(encSeed);
-console.log(encMnemonic);
+  var seedHex = bip39.mnemonicToEntropy(mnemonic);
+  console.log("seed    = " + seedHex);
+  var seed = new Buffer(seedHex, 'hex');
+  var salt = base58.encode(crypto.randomBytes(8));
+  console.log("Salt: " + salt);
+  var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, salt);
+  console.log("Hash: " + pHash.toString('hex'));
+
+  var encSeed = xor(seed, pHash);
+  var encSeed58 = base58.encode(encSeed);
+  console.log("encSeed: " + encSeed.toString('hex'));
+  //console.log("base58  = " + encSeed58);
+
+
+  //qr.toDataURL(encSeed58, function (err, url) {
+  //  console.log(url)
+  //})
+
+  var encMnemonic = bip39.entropyToMnemonic(encSeed);
+  console.log("Mnemonic: " + encMnemonic);
+} else if(mode == 'decrypt') {
+  if(args.length < 4) {
+    usage();
+  }
+  var password = args[1];
+  var salt = args[2];
+  var mnemonic = args[3];
+  var seedHex = bip39.mnemonicToEntropy(mnemonic);
+  console.log("seed = " + seedHex);
+  var seed = new Buffer(seedHex, 'hex');
+  var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, salt);
+  console.log("Hash: " + pHash.toString('hex'));
+  var dSeed = xor(seed, pHash);
+  var dMnemonic = bip39.entropyToMnemonic(dSeed);
+  console.log("Mnemonic: " + dMnemonic);
+
+} else {
+  usage();
+}
