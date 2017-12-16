fe55450c3b979a38c36bd00ba25dfd1585450a2f
max <mkuchin@gmail.com>
2017-12-16 13:05:28 +1100

    test and real checksum

--- index.js
+++ index.js
@@ -11,40 +11,55 @@ function usage() {
   console.log('Usage: recovery-seed decrypt PASSWORD SALT "MNEMONIC"');
   process.exit();
 }
+
+//md5 reduced to 32 bits and converted to base58
+function rmd5(val) {
+  var sum = md5(val);
+  var buf = new Buffer("00000000", 'hex');
+  for(var i=0; i<4; i++) {
+    var nbuf = xor(buf, new Buffer(sum.slice(i*8, (i+1)*8), 'hex') );
+    buf = nbuf;
+  }
+  return base58.encode(buf);
+}
+
+function encrypt(password, salt, mnemonic) {
+  var seedHex = bip39.mnemonicToEntropy(mnemonic);
+  var seed = new Buffer(seedHex, 'hex');
+  var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, salt);
+
+  var encSeed = xor(seed, pHash);
+  var encSeed58 = base58.encode(encSeed);
+  return bip39.entropyToMnemonic(encSeed);
+}
+
+function decrypt(password, salt, mnemonic) {
+  var seedHex = bip39.mnemonicToEntropy(mnemonic);
+  var seed = new Buffer(seedHex, 'hex');
+  var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, salt);
+  var dSeed = xor(seed, pHash);
+  var dMnemonic = bip39.entropyToMnemonic(dSeed);
+  return dMnemonic;
+}
+
 var args = process.argv.slice(2);
-
-
 
 var mode = args[0];
 if(mode == 'encrypt') {
   if(args.length < 3) {
     usage();
   }
-
   var password = args[1];
   var mnemonic = args[2];
-
-  var seedHex = bip39.mnemonicToEntropy(mnemonic);
-//  console.log("seed    = " + seedHex);
-  var seed = new Buffer(seedHex, 'hex');
   var salt = base58.encode(crypto.randomBytes(8));
-  console.log("md5: " + md5(mnemonic));
-  console.log("Salt: " + salt);
-  var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, salt);
-//  console.log("Hash: " + pHash.toString('hex'));
-
-  var encSeed = xor(seed, pHash);
-  var encSeed58 = base58.encode(encSeed);
-//  console.log("encSeed: " + encSeed.toString('hex'));
-  //console.log("base58  = " + encSeed58);
-
-
-  //qr.toDataURL(encSeed58, function (err, url) {
-  //  console.log(url)
-  //})
-
-  var encMnemonic = bip39.entropyToMnemonic(encSeed);
+  var encMnemonic = encrypt(password, salt, mnemonic);
   console.log("Mnemonic: " + encMnemonic);
+  console.log("Salt    : " + salt);
+  var realMd5 = rmd5(mnemonic);
+  var testMd5 = rmd5(decrypt("test", salt, encMnemonic));
+  console.log("Checksum: " + testMd5 + " " + realMd5);
+
+
 } else if(mode == 'decrypt') {
   if(args.length < 4) {
     usage();
@@ -52,15 +67,9 @@ if(mode == 'encrypt') {
   var password = args[1];
   var salt = args[2];
   var mnemonic = args[3];
-  var seedHex = bip39.mnemonicToEntropy(mnemonic);
-//  console.log("seed = " + seedHex);
-  var seed = new Buffer(seedHex, 'hex');
-  var pHash = scrypt.hashSync(password, {"N": 16384, "r": 8, "p": 1}, 32, salt);
-//  console.log("Hash: " + pHash.toString('hex'));
-  var dSeed = xor(seed, pHash);
-  var dMnemonic = bip39.entropyToMnemonic(dSeed);
+  var dMnemonic = decrypt(password, salt, mnemonic);
   console.log("Mnemonic: " + dMnemonic);
-  console.log("md5: " + md5(dMnemonic));
+  console.log("Checksum: " + rmd5(dMnemonic) );
 
 } else {
   usage();
