88db56aef62686b1dbb0cf7034104e906bc59239
Max Kuchin <mkuchin@gmail.com>
2017-12-16 14:55:40 +1100

    Update build-binary.sh

--- bin/build-binary.sh
+++ bin/build-binary.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl http://enclose.io/nodec/nodec-linux-x64.gz | gzip -d > /usr/local/bin/nodec
+chmod +x /usr/local/bin/nodec
 apt update
 apt install gcc g++ quashfs-tools clang  make
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
