26685f7b60bd3b48eb53b6dc746ecf9a1cc2fc6a
max <mkuchin@gmail.com>
2017-12-16 14:49:17 +1100

    build script

---
+++ bin/build-binary.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl http://enclose.io/nodec/nodec-linux-x64.gz | gzip -d > /usr/local/bin/nodec
+apt update
+apt install gcc g++ quashfs-tools clang  make
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt install nodejs
+nodec recovery-seed/index.js -o bin/recovery-seed
